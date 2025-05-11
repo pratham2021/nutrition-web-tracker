c62da65a9217f1cfcad3b5786e023978041c4193
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-05-11 11:00:17 -0700

    First Commit

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,35 @@
-import React from "react";
-import { Utensils } from "lucide-react"
+"use client";
+import React, { useEffect, useState } from "react";
+import { Utensils } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { auth } from "../src/app/firebase/config";
+import { signOut, deleteUser } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { onAuthStateChanged } from "firebase/auth";
+
 
 const NavBar = () => {
+  const [user, setUser] = useState(null);
+  
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        console.log("Logged in!");
+      } else {
+        setUser(null);
+        router.push("/");
+      }
+    });
+  }, []);
+
+  const router = useRouter();
+
+  const logTheUserOut = () => {
+    signOut(auth);
+    router.push("/");
+  };
+
   return (
     <>
       <nav className="border-t-4 border-green-400">
@@ -20,7 +48,7 @@ const NavBar = () => {
                           <a className="text-black hover:bg-white hover:text-green-400 rounded-lg p-2">Meals</a>
                           <a className="text-black hover:bg-white hover:text-green-400 rounded-lg p-2">Analytics</a>
                           <a className="text-black hover:bg-white hover:text-green-400 rounded-lg p-2">Profile</a>
-                          
+                          <a className="text-black hover:bg-white hover:text-green-400 rounded-lg p-2" onClick={logTheUserOut}>Log Out</a>
                       </div>
                   </div>
               </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,29 +1,9 @@
 'use client';
 import LoginForm from "../../components/LoginForm";
-import RegisterForm from "../../components/RegisterForm";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "./firebase/config";
-import { useRouter } from 'next/navigation';
-import { useState, useEffect } from "react";
 
 export default function Home() {
 
-  const [signInPressed, setSignInPressed] = useState(false);
-  const [signUpPressed, setSignUpPressed] = useState(false);
-
-  const [user] = useAuthState(auth);
-
-  const router = useRouter();
-
-  useEffect(() => {
-    if (user) {
-      router.push('/dashboard');
-    }
-  }, [user, router]);
-
   return (
-    <main>
-      {!user && (!signInPressed && !signUpPressed ? (<RegisterForm/>) : (signInPressed ? (<LoginForm/>) : (<RegisterForm/>)))}
-    </main>
+    <LoginForm/>
   );
 }
